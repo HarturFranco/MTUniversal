e7b24cf3797d338ac6b8b7eaa2dff4525cd18756
HarturFranco <73862911+HarturFranco@users.noreply.github.com>
2021-12-04 17:31:59 -0300

    refactor: decodificacao

--- main.py
+++ main.py
@@ -10,16 +10,16 @@ def decode_UTM_text():
     text = ''
     for line in lines:
         line = line.replace('\n', '')
-        print(repr(line))
+        # print(repr(line))
         text = text + line
 
     turing_machine, entrada = text.split('000')[1:-1]
 
     transitions_list = turing_machine.split('00')
-    print(transitions_list)
+    # print(transitions_list)
     
-    max_state = 0;
-    max_input = 0;
+    max_state = 0
+    max_input = 0
     
     transitions = {}
     for t in transitions_list:
@@ -27,10 +27,12 @@ def decode_UTM_text():
         current_state = len(current_state)
         input_symbol = len(input_symbol)
         next_state = len(next_state)
+        print(next_state)
         new_symbol = len(new_symbol)
         direction = len(direction)
         
         max_state = max(max_state, next_state, current_state)
+        print(max_state)
         max_input = max(max_input, input_symbol)
         
         transitions[(current_state, input_symbol)] = (next_state, new_symbol, direction)
@@ -49,14 +51,14 @@ def decode_UTM_text():
 
 def main():
     transitions, tape, max_state, max_input = decode_UTM_text()
+    
     turingMachine = MT(alphabet_symbols= range(1, max_input) , blank= max_input, input_symbols = range(1, max_input), states= range(1, max_state), initial_state=1, transitions=transitions, tape=tape)
 
     while not turingMachine.halted:
-        turingMachine.print_state()
+        turingMachine.print()
         turingMachine.step()
     print("halted")
 
 
 if __name__ == '__main__':
     main()
-
